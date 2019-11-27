9647903bfa63814ace56ee96631fdbc336a91b83
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-11-26 23:49:52 -0300

    inicio modelagem classe Hash

--- main.cpp
+++ main.cpp
@@ -2,11 +2,50 @@
 
 using namespace std;
 
+template <class T>
+class Hash {
+
+private:
+    int size;
+    vector<vector<T>> table;
+    
+    int hashFunction(T s){
+            
+    }
+    
+
+public:
+    Hash(int s){
+        size = s;
+        table.assign(s, vector<T>());
+    }
+    
+    void insert(T s){
+    
+        int pos = hashFunction(s);
+        
+        table[pos].push_back(s);
+    
+    }
+    
+    T search(T s){
+        int pos = hashFunction(s);
+        
+        for(int i=0; i < table[pos].size(); i++){
+            if(s == table[pos][i])
+                return table[pos][i];
+        }
+        return ;
+    }
+
+
+};
+
 
 int main(){
 
-    cout << "hello!" << endl;
-    cout << "olaa" << endl;
+    freopen("out.txt", "w", stdout);
+    freopen("minirating.csv", "r",stdin);
 
     return 0;
 
